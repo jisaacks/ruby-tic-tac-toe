69e6ba15d3ec5bb4e44c5470f9d423b9ec840c15
John Isaacks <john.isaacks@programming-perils.com>
2012-07-01 02:29:24 -0400

    Use new hash syntax

--- tic.rb
+++ tic.rb
@@ -4,9 +4,9 @@ class Tic
   def initialize
     #the tic tac toe slots
     @places = {
-      :a1=>" ",:a2=>" ",:a3=>" ",
-      :b1=>" ",:b2=>" ",:b3=>" ",
-      :c1=>" ",:c2=>" ",:c3=>" "
+      a1:" ",a2:" ",a3:" ",
+      b1:" ",b2:" ",b3:" ",
+      c1:" ",c2:" ",c3:" "
     }
     
     #map of all places that are possible wins
